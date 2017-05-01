14ba554db62239ac1564126c57f859bfdfe55eee
Dmans <dmans1124@gmail.com>
2017-05-01 13:02:15 +0800

    email config

---
+++ application/config/email.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_user'] = 'Diagonalleybg@gmail.com';
+$config['smtp_pass'] = '';
+$config['smtp_timeout'] = 30;
+$config['smtp_port'] = 465;
+$config['charset'] = "utf-8";
+$config['mailtype'] = "html";
+$config['newline'] = "\r\n";
+
+
