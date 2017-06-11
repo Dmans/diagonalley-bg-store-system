d5e9c532044418eb833b518157336e3ebade643f
dmans <dmans@kuande-mbp>
2017-06-11 19:31:13 +0800

    resolved #7 : 移除不必要變數及comment

--- application/controllers/automail/Automail_action.php
+++ application/controllers/automail/Automail_action.php
@@ -5,16 +5,11 @@ class Automail_action extends CI_Controller {
             parent::__construct();
             $this->load->library('form_validation');
             $this->load->model("automail/automail_service");
-            // $this->load->model('service/store_data_service');
-
         }
 
 
     public function afternoon($valid, $send=FALSE){
 
-//         $receiver = array('lijia.yeh@gmail.com', 'gpsam25@gmail.com', 'dmans1124@gmail.com');
-
-        // $receiver = array('dmans1124@gmail.com');
     	$data = $this->automail_service->get_automail_data();
     	
         if($valid == 'fmweeije45gEEFGerr') {
